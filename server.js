eae13f794f2a83cb435ee6778465fac819532cfc
BPM <brantleymorris@me.com>
2020-12-21 19:33:33 -0600

    add routes

--- server.js
+++ server.js
@@ -4,5 +4,31 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var tables = [];
+// arrays for current tables and waiting list
+var serve = [];
+var wait = [];
 
+// routes to send pages
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/make", function(req, res) {
+    res.sendFile(path.join(__dirname, "make.html"));
+});
+
+app.get("/view", function (req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+})
+
+// routes to post data
+app.post("/make", function(req, res) {
+    var newRes = req.body;
+
+    if (serve.length < 5) {
+        serve.push(newRes);
+    }
+    else {
+        wait.push(newRes);
+    }
+})
